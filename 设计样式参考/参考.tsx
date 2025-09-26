36ceb16c4d1cfe21ef034c2513cde40216770dbc
Your Name <your-email@example.com>
2025-09-27 05:05:25 +0800

    feat(轮播组件): 添加高性能轮播组件并更新UI样式
    refactor(产品卡片): 优化产品卡片样式和布局
    
    style(全局): 统一卡片阴影和边框样式
    
    chore(依赖): 更新astro及相关依赖版本
    
    fix(Popup): 增强width属性验证逻辑
    
    docs(样式参考): 添加腾讯和本项目边框样式参考文件

---
+++ 设计样式参考/参考.tsx
@@ -0,0 +1,65 @@
+    --swiper-theme-color: #007aff;
+    --swiper-navigation-size: 44px;
+    font-size: 14px;
+    font-family: PingFangSC-Regular, PingFang SC;
+    font-weight: 400;
+    cursor: pointer;
+    list-style: none;
+    border: 0;
+    margin: 0;
+    padding: 0 90px;
+    display: flex;
+    justify-content: space-between;
+    padding-top: 54px;
+    box-sizing: border-box;
+    margin-top: 0;
+
+
+        --swiper-theme-color: #007aff;
+    --swiper-navigation-size: 44px;
+    font-size: 14px;
+    font-family: PingFangSC-Regular, PingFang SC;
+    font-weight: 400;
+    list-style: none;
+    border: 0;
+    margin: 0;
+    text-decoration: none;
+    color: #333;
+    outline: 0;
+    background: rgba(255, 255, 255, 0.60);
+    backdrop-filter: blur(16px);
+    display: flex;
+    box-sizing: border-box;
+    width: 340px;
+    height: 122px;
+    padding: 16px 24px;
+    border-radius: 8px;
+
+
+
+    .navs li.navs-li .ernav .shadow_solution .content .news_solution {
+    background: rgba(255, 255, 255, 0.60);
+    backdrop-filter: blur(16px);
+    display: flex;
+    /* align-items: center; */
+    box-sizing: border-box;
+    width: 340px;
+    height: 122px;
+    padding: 16px 24px;
+    border-radius: 8px;
+}
+
+        --swiper-theme-color: #007aff;
+    --swiper-navigation-size: 44px;
+    font-size: 14px;
+    font-family: PingFangSC-Regular, PingFang SC;
+    font-weight: 400;
+    cursor: pointer;
+    list-style: none;
+    padding: 0;
+    border: 0;
+    margin: 0;
+    display: grid;
+    grid-template-columns: repeat(3, 340px);
+    grid-column-gap: 40px;
+    grid-row-gap: 24px;
